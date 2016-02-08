6f621e11352e9398e54c7bb456c0a9a7f0f5c7eb
Chong-U <me@chongdashu.com>
2016-02-07 20:19:45 -0500

    Update tests

--- src/test/scala/practice/sort/TestSort.scala
+++ src/test/scala/practice/sort/TestSort.scala
@@ -17,29 +17,57 @@ class TestSort extends TestCase{
     var smallIntegerListRandom = List(4,5,2,3,1);
     var smallIntegerListSorted = List(1,2,3,4,5);
     var smallIntegerListAllSame = List(7,7,7,7,7);
+    var smallIntegerListSomeSame = List(7,1,5,7,3);
+    var smallIntegerListSomeSameSorted = List(1,3,5,7,7);
     
+    /**
+     * Sorting an empty list should just return the empty list.
+     */
     def testSort_EmptyList() = {
         assertEquals(List.empty, Sort.sort(List.empty));
     }
     
+    /**
+     * Sorting a list with a single element should just return the
+     * same list.
+     */
     def testSort_SingleElementList() = {
         assertEquals(singleIntegerList, Sort.sort(singleIntegerList));
     }
     
+    /**
+     * Sort a small list that is in reverse order.
+     */
     def testSort_SmallList_Reversed() = {
         assertEquals(smallIntegerListSorted, Sort.sort(smallIntegerListReversed));
     }
     
+    /**
+     * Sort a small list that is already sorted.
+     */
     def testSort_SmallList_Sorted() = {
         assertEquals(smallIntegerListSorted, Sort.sort(smallIntegerListSorted));
     }
     
+    /**
+     * Sort a small list with elements randomly interspersed.
+     */
     def testSort_SmallList_Random() = {
         assertEquals(smallIntegerListSorted, Sort.sort(smallIntegerListRandom));
     }
     
-      def testSort_SmallList_AllSame() = {
+    /**
+     * Sort a small list with all elements having the same value.
+     */
+    def testSort_SmallList_AllSame() = {
         assertEquals(smallIntegerListAllSame, Sort.sort(smallIntegerListAllSame));
+    }
+    
+    /**
+     * Sort a small list with all elements having the same value.
+     */
+    def testSort_SmallList_SomeSame() = {
+        assertEquals(smallIntegerListSomeSameSorted, Sort.sort(smallIntegerListSomeSame));
     }
 
 }
