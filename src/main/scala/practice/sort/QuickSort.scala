b462c0fedd7222d3551947f0eda6539939b28af3
Chong-U <me@chongdashu.com>
2016-02-07 20:11:44 -0500

    Implement quicksort

--- src/main/scala/practice/sort/QuickSort.scala
+++ src/main/scala/practice/sort/QuickSort.scala
@@ -1,9 +1,44 @@
 package com.chongdashu.scala.practice.sort
 
+import scala.collection.mutable.ListBuffer
+
 object QuickSort extends BaseSort {
     
-    def sort(list : List[Int]) : List[Int] = {
-        return List();
+    def sort( list : List[Int]) : List[Int] = {
+        
+        // Base case
+        if (list.isEmpty || list.size == 1) {
+             return list;
+        }
+        
+        val listBuffer : ListBuffer[Int] = ListBuffer.empty ++ list;
+        
+        // Step (1): Randomly select a pivotIndex and pivot
+        //           Usually it's the last element
+        var pivotIndex : Int = listBuffer.length - 1;
+        var pivotValue = listBuffer(pivotIndex);
+        
+        val leftOfPivot : ListBuffer[Int] = ListBuffer.empty;
+        val rightOfPivot : ListBuffer[Int] = ListBuffer.empty
+        val centerOfPivot : ListBuffer[Int] = ListBuffer.empty;
+        
+        var newPivotIndex = -1;
+        
+        for ((element, index) <- listBuffer.zipWithIndex) {
+            if (element < pivotValue) {
+                leftOfPivot += element;
+            }
+            else if (element > pivotValue) {
+                rightOfPivot += element;
+            }
+            else {
+                centerOfPivot += element;
+            }
+        }
+        
+        return  (leftOfPivot ++ centerOfPivot).toList ++ sort(rightOfPivot.toList)
+        
+        
     }
 }
 
--- src/test/scala/practice/sort/TestSort.scala
+++ src/test/scala/practice/sort/TestSort.scala
@@ -5,33 +5,41 @@ import org.junit.Assert._
 import com.chongdashu.scala.practice.sort.BaseSort
 import com.chongdashu.scala.practice.sort.QuickSort
 import com.chongdashu.scala.practice.sort.BubbleSort
+import junit.framework.Test
 
 class TestSort extends TestCase{
     
-    var Sort : BaseSort = BubbleSort;
+    var Sort : BaseSort = BubbleSort
     
-    def testSort_ListReversed() = {
-        val integerList = List(3,2,1);
-        val expectedIntegerList = List(1,2,3);
-        
-        val sortedIntegerList = Sort.sort(integerList);
-        assertEquals(expectedIntegerList, sortedIntegerList);
+    var singleIntegerList = List(1);
+    
+    var smallIntegerListReversed = List(5,4,3,2,1);
+    var smallIntegerListRandom = List(4,5,2,3,1);
+    var smallIntegerListSorted = List(1,2,3,4,5);
+    var smallIntegerListAllSame = List(7,7,7,7,7);
+    
+    def testSort_EmptyList() = {
+        assertEquals(List.empty, Sort.sort(List.empty));
     }
     
-    def testSort_ListSorted() = {
-        val integerList = List(1,2,3);
-        val expectedIntegerList = List(1,2,3);
-        
-        val sortedIntegerList = Sort.sort(integerList);
-        assertEquals(expectedIntegerList, sortedIntegerList);
+    def testSort_SingleElementList() = {
+        assertEquals(singleIntegerList, Sort.sort(singleIntegerList));
     }
     
-    def testSort_ListRandom() = {
-        val integerList = List(3,1,2);
-        val expectedIntegerList = List(1,2,3);
-        
-        val sortedIntegerList = Sort.sort(integerList);
-        assertEquals(expectedIntegerList, sortedIntegerList);
+    def testSort_SmallList_Reversed() = {
+        assertEquals(smallIntegerListSorted, Sort.sort(smallIntegerListReversed));
+    }
+    
+    def testSort_SmallList_Sorted() = {
+        assertEquals(smallIntegerListSorted, Sort.sort(smallIntegerListSorted));
+    }
+    
+    def testSort_SmallList_Random() = {
+        assertEquals(smallIntegerListSorted, Sort.sort(smallIntegerListRandom));
+    }
+    
+      def testSort_SmallList_AllSame() = {
+        assertEquals(smallIntegerListAllSame, Sort.sort(smallIntegerListAllSame));
     }
 
 }
