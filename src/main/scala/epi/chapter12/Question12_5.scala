5e34a36c6e2e8400a909439ee312c6ad63305818
Chong-U <me@chongdashu.com>
2016-02-22 19:46:11 -0500

    [EPI] Add solution to 12.5

--- src/main/scala/epi/chapter12/Question12_5.scala
+++ src/main/scala/epi/chapter12/Question12_5.scala
@@ -11,6 +11,27 @@ object Question12_5 {
      * @return integer `x` where `x<sup>2</sup> &le; value`  
      */
     def squareRoot(value : Int) : Int =  {
-        return -1;
+        if (value <= 1) {
+            return value;
+        }
+        
+        // Crete an interval [left, right]
+        // All values x <= left will be x*x <= value
+        // All values y > right will be y*y > value
+        var left = 0;
+        var right = value;
+        
+        while (left <= right) {
+            var mid = (left + right) / 2;
+            var midSq = mid*mid;
+            if (midSq <= value) {
+                left += 1;
+            }
+            else {
+                right -= 1;
+            }
+        }
+        
+        return left-1;
     }
 }
--- src/test/scala/epi/chapter12/TestQuestion12_5.scala
+++ src/test/scala/epi/chapter12/TestQuestion12_5.scala
@@ -9,6 +9,41 @@ class TestQuestion12_5 extends TestCase {
     override def setUp() = {
     }
     
+    def testSquareRootZero = {
+        val value = 0;
+        val expected = 0;
+        val actual = Question12_5.squareRoot(value);
+        assertEquals(s"For $value: $expected^2=${expected*expected}; but $actual^2=${actual*actual}", expected, actual);
+    }
+    
+    def testSquareRootOne = {
+        val value = 1;
+        val expected = 1;
+        val actual = Question12_5.squareRoot(value);
+        assertEquals(s"For $value: $expected^2=${expected*expected}; but $actual^2=${actual*actual}", expected, actual);
+    }
+    
+    def testSquareRootTwo = {
+        val value = 2;
+        val expected = 1;
+        val actual = Question12_5.squareRoot(value);
+        assertEquals(s"For $value: $expected^2=${expected*expected}; but $actual^2=${actual*actual}", expected, actual);
+    }
+    
+    def testSquareRootThree = {
+        val value = 3;
+        val expected = 1;
+        val actual = Question12_5.squareRoot(value);
+        assertEquals(s"For $value: $expected^2=${expected*expected}; but $actual^2=${actual*actual}", expected, actual);
+    }
+    
+    def testSquareRootFour = {
+        val value = 4;
+        val expected = 2;
+        val actual = Question12_5.squareRoot(value);
+        assertEquals(s"For $value: $expected^2=${expected*expected}; but $actual^2=${actual*actual}", expected, actual);
+    }
+    
     def testBookExampleOne {
         var value = 16;
         var expected = 4;
